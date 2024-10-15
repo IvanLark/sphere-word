5b27389d276cea883cf63779635b127f41d25fd9
Woisol-G <Woisol-G@outlook.com>
2024-10-15 19:06:39 +0800

    build, style: 导入MUI，使用Accordion实现单词详情展示

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,14 +1,30 @@
 import * as React from 'react';
-export default function Accordion({ title, child, titleClass = "bg-teal-500 text-white", childClass = "bg-gray-100" }: { title: string, child: JSX.Element, titleClass: string, childClass: string }) {
+import { AccordionDetails, AccordionSummary, Accordion as MuiAccordion } from '@mui/material';
+import { ExpandMoreRounded } from '@mui/icons-material';
+export default function Accordion_word_core({ title, child }: { title: string, child: JSX.Element }) {
 	// !对不起以为这个React.HtmlHTMLAttributes<HTMLDivElement>.className?是类型……
 	const [isOpen, setIsOpen] = React.useState(false);
 	return (
-		<div className={`w-full rounded-3xl overflow-hidden ${childClass}`} onClick={() => { if (!isOpen) setIsOpen(true) }}>
-			<p className={`w-full p-4 pb-2 relative text-2xl font-bold rounded-3xl overflow-hidden ${titleClass} `} onClick={() => setIsOpen(!isOpen)}>{title}<span className={`absolute right-8 text-3xl font-bold transition-transform ${isOpen ? '-rotate-90' : ''}`}>&lt;</span></p>
-			<div className={` px-4 p-4 text-ellipsis transition-all duration-300 ${isOpen ? 'max-h-screen' : ' max-h-28'}`}>{child}</div>
-			{/* <child className={`text-ellipsis transition-all ${isOpen ? '' : 'max-h-6'} ${childClass}`} /> */}
-			{/* //td @IvanLark 因为用了child无法使用text-ellipsis，所以擅自主张加了这种效果，看看如何 */}
-			<div className={`w-full h-16 relative z-10 bg-gradient-to-t from-black to-transparent transition-all duration-300 ${isOpen ? 'opacity-0' : 'opacity-50'}`}><span className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>还有更多内容</span></div>
-		</div>
+		// <div className={`w-full rounded-3xl overflow-hidden ${childClass}`} onClick={() => { if (!isOpen) setIsOpen(true) }}>
+		// 	<p className={`w-full p-4 pb-2 relative text-2xl font-bold rounded-3xl overflow-hidden ${titleClass} `} onClick={() => setIsOpen(!isOpen)}>{title}<span className={`absolute right-8 text-3xl font-bold transition-transform ${isOpen ? '-rotate-90' : ''}`}>&lt;</span></p>
+		// 	<div className={` px-4 p-4 text-ellipsis transition-all duration-300 ${isOpen ? 'max-h-screen' : ' h-0'}`} style={{ height: isOpen ? `${child.offsetHeight}px` : '0' }}>{child}</div>
+		// 	{/* <child className={`text-ellipsis transition-all ${isOpen ? '' : 'max-h-6'} ${childClass}`} /> */}
+		// 	{/* //dtd @IvanLark 因为用了child无法使用text-ellipsis，所以擅自主张加了这种效果，看看如何 */}
+		// 	{/* <div className={`w-full h-16 relative z-10 bg-gradient-to-t from-black to-transparent transition-all duration-300 ${isOpen ? 'opacity-0' : 'opacity-50'}`}><span className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>还有更多内容</span></div> */}
+		// </div>
+		<MuiAccordion style={{ borderRadius: '10px' }}>
+			{/* className={`w-full rounded-3xl overflow-hidden ${childClass}`} */}
+			<AccordionSummary
+				expandIcon={<ExpandMoreRounded />}
+				style={{ color: 'rgb(253,224,71)', fontWeight: 'bold', backgroundColor: 'rgb(96,160,250)', borderRadius: '10px' }}
+				// className={`p-4 pb-2 relative text-2xl font-bold rounded-3xl overflow-hidden ${titleClass} `}
+				onClick={() => setIsOpen(!isOpen)}
+			>
+				{title}
+			</AccordionSummary>
+			<AccordionDetails >
+				<div className={` px-4 p-4 text-ellipsis transition-all duration-300`} >{child}</div>
+			</AccordionDetails>
+		</MuiAccordion >
 	)
 }
