3c5054559b34b8af3e7d99d5e5f446fc330bc147
IvanLark <3149107774@qq.com>
2024-11-10 17:22:00 +0800

    小修改

--- src/pages/review/pages/review-word-list.tsx
+++ src/pages/review/pages/review-word-list.tsx
@@ -14,7 +14,6 @@ export default function ReviewWordList({ index, words, onClose, open }: ReviewWo
       className={`w-full max- h-[calc(100vh-4rem)] p-5 overflow-y-scroll fixed z-10 bottom-0 bg-white flex flex-col
       items-center transition-opacity duration-300 ${open ? "opacity-100" : "opacity-0 pointer-events-none"}`}
       onClick={onClose}>
-      <h2 className="w-full text-xl font-bold text-center mb-2">剩余单词</h2>
       {
         words.map((wordData, mapIndex) =>
           <button key={mapIndex} className={`btn-scale btn-trans w-full py-4 text-center rounded-lg 
