740982662c0595936a9ba3251510eb7706640cd1
Woisol-G <Woisol-G@outlook.com>
2024-11-10 22:03:24 +0800

    fix, style: 修复登录页面是北邮人时输入框被遮挡的问题，修改了北邮人的checkbox样式

--- src/common/components/input.tsx
+++ src/common/components/input.tsx
@@ -3,13 +3,29 @@ import {ArrowForward} from "@mui/icons-material";
 
 interface InputProps {
   label: string;
-  type: 'text' | 'number' | 'password';
+  type: 'text' | 'checkbox-slide' | 'number' | 'password';
   required: boolean;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string | boolean;
+  onChange: (value: string | boolean) => void;
 }
 
 export default function Input({ label, type, required, value, onChange }: InputProps) {
+  if (type === 'checkbox-slide') {
+    return (
+      <div className="w-fit h-12 flex items-center rounded-md border-2 border-black self-start">
+        {/* label */}
+        <div className="w-20 py-2 h-full text-center text-lg border-r-2 border-black">
+          {label}
+        </div>
+        {/* input */}
+        <div className=" h-12 relative flex items-center">
+          <div className={`w-16 h-full bg-black pointer-events-none absolute top-0 transition-all duration-300 ${value ? 'left-16' : 'left-0'}`}></div>
+          <span className={`w-16 h-full py-3 relative text-center ${value ? '' : 'text-white'}`} onClick={() => onChange(false)}>否</span>
+          <span className={`w-16 h-full py-3 relative text-center ${value ? 'text-white' : ''}`} onClick={() => onChange(true)}>是</span>
+        </div>
+      </div>
+    )
+  }
   return (
     <div className="w-full pr-2 flex items-center rounded-md border-2 border-black">
       {/* label */}
@@ -17,8 +33,8 @@ export default function Input({ label, type, required, value, onChange }: InputP
         {label}
       </div>
       {/* input */}
-      <input title={label} type={type} required={required} value={value} onChange={onChange}
-             className=" flex-1 pl-2 text-lg rounded-md" />
+      <input title={label} type={type} required={required} value={value as string} onChange={(event) => onChange(event.target.value)}
+        className=" flex-1 pl-2 text-lg rounded-md" />
     </div>
   );
 }
